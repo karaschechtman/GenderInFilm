bdde989e7b558d910c1b664e2d1b65fbb4dc1ad2
karaschechtman <kws2121@columbia.edu>
2019-01-16 09:47:09 -0500

    Add character line data to Movie object and create Character object

--- Character.py
+++ Character.py
@@ -5,7 +5,12 @@ class Character(object):
     """
     Stores data and metadata about a particular chracter,
     associating lines to each character.
+    Name stores the name of the character.
+    Line data is a list of ints. Each entry represents a line
+    and has a value equal to the number of words in that line.
+    Gender is set to None by default.
     """
-    def __init__(self, character_name, lines):
-        self.character_name = character_name
-        self.lines = lines
+    def __init__(self, name, line_data):
+        self.name = name
+        self.line_data = line_data
+        self.gender = None
--- Movie.py
+++ Movie.py
@@ -1,6 +1,12 @@
 __author__ = "Kara Schechtman <kws2121@columbia.edu>"
 __date__ = "Jan 15, 2019"
 
+from Character import Character
+from collections import defaultdict
+from name_processing import variant_to_root
+
+CHARACTER = 'C|'
+DIALOGUE = 'D|'
 
 class Movie(object):
     """
@@ -18,3 +24,23 @@ class Movie(object):
         self.rating = rating
         self.bechdel_score = bechdel_score
         self.imdb_cast = imdb_cast
+        self.characters = []
+
+        # Create a mapping from characters to their lines.
+        name = None
+        dialogue = ''
+        names = defaultdict(list)
+        for datum in script:
+            if datum.startswith(CHARACTER):
+                name = variant_to_root(datum.split(CHARACTER)[1].strip())
+                names[name].append(dialogue.strip())
+                dialogue = ''
+            if datum.startswith(DIALOGUE):
+                dialogue += ' ' + datum.split(DIALOGUE)[1].strip()
+
+        # Create and save character objects.
+        for name in names:
+            line_data = []
+            for line in names[name]:
+                line_data.append(len(line.split()))
+            self.characters.append(Character(name, line_data))
