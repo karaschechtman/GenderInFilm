5b7c3e8354a87e584878057d61c4e5c308267ae2
karaschechtman <kws2121@columbia.edu>
2019-01-19 18:06:54 -0500

    Add a hard backtracking mode to assignments; move around functions in imdb_matching to have a testing section

--- imdb_matching.py
+++ imdb_matching.py
@@ -59,6 +59,137 @@ def blended_align(iname, sname, threshold=THRESHOLD):
         return True
     return False
 
+# --------------------------- ASSIGNMENT --------------------------
+def _hard_backtrack(script_to_imdb, assignments):
+    """
+    Recursive helper function for hard backtracking.
+    """
+    # Check completion condition.
+    if not script_to_imdb:
+        return assignments
+
+    # Choose variable to assign according to MRV heuristic.
+    sname = max(script_to_imdb, key=lambda k: len(script_to_imdb[k]))
+
+    # Check failure condition.
+    if len(script_to_imdb[sname]) == 0:
+        return False
+
+    # Order potential assignments according to LCV heuristic.
+    ordered_inames = []
+    for iname in script_to_imdb[sname]:
+        num_occurences = sum([1 for s in script_to_imdb if iname in script_to_imdb[s]])
+        ordered_inames.append((num_occurences, iname))
+    ordered_inames.sort()
+
+    # Try the assignment.
+    for iname in ordered_inames:
+        # Create the new script to IMDB mapping .
+        new_script_to_imdb = script_to_imdb.copy()
+        del new_script_to_imdb[sname]
+        # Delete iname as a potential match.
+        for s in new_script_to_imdb:
+            if iname[1] in new_script_to_imdb[s]:
+                new_script_to_imdb[s].remove(iname[1])
+        assignments[sname] = iname[1]
+        backtracked_assignments = _hard_backtrack(new_script_to_imdb, assignments)
+        if backtracked_assignments != False:
+            return backtracked_assignments
+    return False
+
+def hard_backtrack(lines, alignment_fn):
+    """
+    Match characters in the script to IMDB characters
+    for any characters that have IMDB matches.
+
+    Character assignment is formulated as a constraint
+    satisfaction problem, with one script character starting
+    with a number of potential IMDB matches that will
+    be whittled down to one unique match.
+    """
+    cast = lines[7]
+    inames = _get_imdb_gender_mapping(lines[7])
+    script_to_imdb = defaultdict(list)
+
+    # Align script characters with IMDB matches to possible
+    # IMDB characters.
+    for line in lines[9:]:
+        sname = line.split(':')[0]
+        for iname in inames:
+            if alignment_fn(iname, sname):
+                script_to_imdb[sname].append(iname)
+
+    # Run CSP to match characters to names.
+    assignments_init = dict.fromkeys(script_to_imdb.keys())
+    assignments = _hard_backtrack(script_to_imdb, assignments_init)
+    if assignments:
+        return assignments
+    else:
+        return False
+
+def _soft_backtrack(script_to_imdb, assignments):
+    """
+    Recursive helper function for soft backtracking.
+    """
+    # Check completion condition.
+    if not script_to_imdb:
+        return assignments
+
+    # Choose variable to assign according to MRV heuristic.
+    sname = max(script_to_imdb, key=lambda k: len(script_to_imdb[k]))
+
+    # Order potential assignments according to LCV heuristic.
+    # This enforces a soft preference for matching snames
+    # to unique inames (repeated iname assignment is not
+    # expressly forbidden).
+    ordered_inames = []
+    for iname in script_to_imdb[sname]:
+        num_occurences = sum([1 for s in script_to_imdb if iname in script_to_imdb[s]])
+        ordered_inames.append((num_occurences, iname))
+    ordered_inames.sort()
+
+    # Try the assignment.
+    for iname in ordered_inames:
+        del script_to_imdb[sname]
+        assignments[sname] = iname[1]
+        backtracked_assignments = _soft_backtrack(script_to_imdb, assignments)
+        if backtracked_assignments != False:
+            return backtracked_assignments
+    return False
+
+def soft_backtrack(lines, alignment_fn):
+    """
+    Match characters in the script to IMDB characters
+    for any characters that have IMDB matches.
+
+    Character assignment is formulated as a constraint
+    satisfaction problem, with one script character starting
+    with a number of potential IMDB matches that will
+    be whittled down to one match. This "backtracking" will
+    never actually backtrack, since all the constraints
+    are soft and approximated through heuristics.
+    """
+    cast = lines[7]
+    inames = _get_imdb_gender_mapping(lines[7])
+    script_to_imdb = defaultdict(list)
+
+    # Align script characters with IMDB matches to possible
+    # IMDB characters.
+    for line in lines[9:]:
+        sname = line.split(':')[0]
+        for iname in inames:
+            if alignment_fn(iname, sname):
+                script_to_imdb[sname].append(iname)
+
+    # Run CSP to match characters to names.
+    assignments_init = dict.fromkeys(script_to_imdb.keys())
+    assignments = _soft_backtrack(script_to_imdb, assignments_init)
+    if assignments:
+        return assignments
+    else:
+        return False
+
+# ---------------------------- TESTING ----------------------------
 def _count_matches_per_script(lines, alignment_fn):
     """
     Helper for test_alignment_coverage to count matches
@@ -123,72 +254,13 @@ def test_alignment_coverage(alignment_fn, verbose=False):
                                               round(total_lines_missed/total_lines * 100, 2)))
     print('----------------------------')
 
-# --------------------------- ASSIGNMENT --------------------------
-def _backtrack(script_to_imdb, assignments):
-    """
-    Recursive helper function for backtracking.
-    """
-    # Check completion condition.
-    if not script_to_imdb:
-        return assignments
-
-    # Choose variable to assign according to MRV heuristic.
-    sname = max(script_to_imdb, key=lambda k: len(script_to_imdb[k]))
-
-    # Check failure condition.
-    if len(script_to_imdb[sname]) == 0:
-        return False
-
-    # Order potential assignments according to LCV heuristic.
-    # This enforces a soft preference for matching snames
-    # to unique inames (repeated iname assignment is not
-    # expressly forbidden).
-    ordered_inames = []
-    for iname in script_to_imdb[sname]:
-        num_occurences = sum([1 for s in script_to_imdb if iname in script_to_imdb[s]])
-        ordered_inames.append((num_occurences, iname))
-    ordered_inames.sort()
-
-    # Try the assignment.
-    for iname in ordered_inames:
-        del script_to_imdb[sname]
-        assignments[sname] = iname
-        backtracked_assignments = _backtrack(script_to_imdb, assignments)
-        if backtracked_assignments != False:
-            return backtracked_assignments
-    return False
-
-def backtrack(lines, alignment_fn):
-    """
-    Match characters in the script to IMDB characters
-    using backtracking for any characters that have IMDB
-    matches.
-
-    Character assignment is formulated as a constraint
-    satisfaction problem, with one script character starting
-    with a number of potential IMDB matches that will
-    be whittled down to one match.
-    """
-    cast = lines[7]
-    inames = _get_imdb_gender_mapping(lines[7])
-    script_to_imdb = defaultdict(list)
-
-    # Align script characters with IMDB matches to possible
-    # IMDB characters.
-    for line in lines[9:]:
-        sname = line.split(':')[0]
-        for iname in inames:
-            if alignment_fn(iname, sname):
-                script_to_imdb[sname].append(iname)
-
-    # Run CSP to match characters to names.
-    assignments = dict.fromkeys(script_to_imdb.keys())
-    if _backtrack(script_to_imdb, assignments):
-        return assignments
-    else:
-        return False
-
 def test_assignment_coverage(assignment_fn, alignment_fn, verbose=False):
+    """
+    Tests coverage of assignments from alignments.
+    Provides four statistics:
+    1. Number of files matched.
+
+    """
     total_success = 0
     total_failure = 0
     for fn in os.listdir(PATH_TO_DATA):
@@ -214,6 +286,9 @@ if __name__ == "__main__":
     test_alignment_coverage(in_align)
     test_alignment_coverage(threshold_align)
     test_alignment_coverage(blended_align)
-    test_assignment_coverage(backtrack, in_align)
-    test_assignment_coverage(backtrack, threshold_align)
-    test_assignment_coverage(backtrack, blended_align)
+    test_assignment_coverage(soft_backtrack, in_align)
+    test_assignment_coverage(soft_backtrack, threshold_align)
+    test_assignment_coverage(soft_backtrack, blended_align)
+    test_assignment_coverage(hard_backtrack, in_align)
+    test_assignment_coverage(hard_backtrack, threshold_align)
+    test_assignment_coverage(hard_backtrack, blended_align)
