4b18e47292edcc304a3c9d8d2fc8f678bd0f4c7c
karaschechtman <kws2121@columbia.edu>
2019-02-02 18:29:25 -0500

    add predict gender function and add workshop scaffold

---
+++ gender_predict.py
@@ -0,0 +1,40 @@
+from imdb_matching import predict_gender_imdb
+from ssa_matching import predict_gender_ssa
+from imdb_matching import *
+from ssa_matching import *
+
+def predict_gender(movie):
+    """
+    Given a movie object, predict the gender of all characters.
+    Returns a dictionary matching from character names to genders.
+    If a charater's gender cannot be assigned with 
+    confidence, the character name will not appear in the dictionary.
+    """
+    imdb_pred_dict = predict_gender_imdb(movie, alignment_fn=in_align, assignment_fn=soft_backtrack)
+    ssa_dict = make_ssa_dict()
+    ssa_pred_dict = predict_gender_ssa(ssa_dict, movie, mode='hard', check_decade=True)
+    pred_dict = _merge_dict(ssa_pred_dict, imdb_pred_dict, True)
+    ordered_snames = sorted(list(pred_dict.keys()))
+    return pred_dict
+
+def _merge_dict(ssa_pred_dict, imdb_pred_dict, ssa_trumps=True):
+    ssa_pred_names = set()
+    for sname, gen in ssa_pred_dict.items():
+        if gen == 'M' or gen == 'F':
+            ssa_pred_names.add(sname)
+    imdb_pred_names = set()
+    for sname, gen in imdb_pred_dict.items():
+        if gen == 'M' or gen == 'F':
+            imdb_pred_names.add(sname)
+    merged_dict = {}
+    for sname in set(ssa_pred_names).union(imdb_pred_names):
+        if sname in ssa_pred_names and sname not in imdb_pred_names:
+            merged_dict[sname] = ssa_pred_dict[sname]
+        elif sname not in ssa_pred_names and sname in imdb_pred_names:
+            merged_dict[sname] = imdb_pred_dict[sname]
+        elif sname in ssa_pred_names and sname in imdb_pred_names:
+            if ssa_trumps:
+                merged_dict[sname] = ssa_pred_dict[sname]
+            else:
+                merged_dict[sname] = imdb_pred_dict[sname]
+    return merged_dict
--- workshop.py
+++ workshop.py
@@ -1,3 +1,74 @@
 from data_loader import DataLoader
-from imdb_matching import predict_gender_imdb
-# from ssa_matching import predict_gender_ssa
+from gender_predict import predict_gender
+
+
+def assign_genders(data):
+    """
+    EXERCISE 1.
+    For every movie in the DataLoader, call predict_gender and
+    assign the returned genders to the Character objects'
+    gender fields.
+    """
+    return None # Delete this when you're done.
+
+def get_top_n_billed_by_gender(data, n):
+    """
+    EXERCISE 2.
+    Given a positive integer n, return a dictionary mapping
+    genders to movies where the top n billed are all of that
+    gender. Dictionary keys should be strings ('M' or 'F')
+    and dictionary values should be lists of the titles
+    of the movies with the top n billed of that gender.
+    If there are no top n billed, the value should be an
+    empty list.
+    """
+    return {"M":[], "F":[]}
+
+def _print_gender_counts(data):
+    """
+    Helper function to print results for exercise 1.
+    """
+    m = 0
+    f = 0
+    for movie in data.movies:
+        mov_obj = data.movies[movie]
+        for character in mov_obj.characters:
+            char_obj = mov_obj.get_character(character)
+            if char_obj.gender == 'M':
+                m+=1
+            elif char_obj.gender == 'F':
+                f+=1
+    print("Number of male characters: %d" % m)
+    print("Number of female characters: %d" % f)
+
+def _print_top_n_billed(top_n_billed, n):
+    """
+    Helper function to print results for exercise 2.
+    """
+    if len(top_n_billed['M']) == 0:
+        print("No movies with top %d male billed!" % n)
+    else:
+        print("Movies with top %d male billed:" % n)
+        for movie in top_n_billed['M']:
+            print ("- %s" % movie)
+    if len(top_n_billed['F']) == 0:
+        print("No movies with top %d female billed!" % n)
+    else:
+        print("Movies with top %d female billed:" % n)
+        for movie in top_n_billed['F']:
+            print ("- %s" % movie)
+
+
+if __name__ == "__main__":
+    data = DataLoader(verbose=False)
+
+    print("EXERCISE 1")
+    assign_genders(data)
+    _print_gender_counts(data)
+    print("----------------------------")
+    
+    print("EXERCISE 2")
+    n = 5 # Feel free to change this number.
+    top_n_billed = get_top_n_billed_by_gender(data, n)
+    _print_top_n_billed(top_n_billed, n)
+    print("----------------------------")
